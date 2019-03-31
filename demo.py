0089b8a07d3061ba0fa1e6810fb2b0576378ff84
luyh <yanhonglu5@icloud.com>
2019-03-31 21:43:37 +0800

    demo 显示北京时间

--- demo.py
+++ demo.py
@@ -3,7 +3,6 @@ from datetime import datetime
 
 cst_tz = timezone('Asia/Shanghai')
 utc_tz = timezone('UTC')
-utcnow = datetime.utcnow()
-utcnow = utcnow.replace(tzinfo=utc_tz)
-china = utcnow.astimezone(cst_tz)
+utcnow = datetime.utcnow().replace(tzinfo=utc_tz)
+china = utcnow.astimezone(cst_tz).strftime('%Y-%m-%d %H:%M:%S')
 print(china,'python demo.py')
